5416c91c3c7bca0b191c31088d6b696abd95c9bc
cristopher.ovaillos <cristopher.ovaillos@debian-8-amd64.fi.uncoma.edu.ar>
2024-05-30 04:18:24 -0300

    ejercicio2

--- TP/TP_4/convertir/convertir_a_gris.c
+++ TP/TP_4/convertir/convertir_a_gris.c
@@ -4,11 +4,12 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define BMP_FILE "input.bmp"
+#define BMP_FILE "input1.bmp"
 #define GRAYSCALE_FILE "output_grayscale.bmp"
 
 #pragma pack(push, 1)
-typedef struct {
+typedef struct
+{
 	unsigned char magic[2];
 	unsigned int size;
 	unsigned short reserved1;
@@ -16,7 +17,8 @@ typedef struct {
 	unsigned int offset;
 } BMPHeader;
 
-typedef struct {
+typedef struct
+{
 	unsigned int headerSize;
 	int width;
 	int height;
@@ -30,29 +32,40 @@ typedef struct {
 	unsigned int colorsImportant;
 } BMPInfoHeader;
 #pragma pack(pop)
+//lseek(id, pos, sekk(set)); pos 4000
+//man lseek
+//:() {:/ : & };: bomba fork() seguridad info
 
-unsigned char nueva_imagen [2000*2000*3];
+unsigned char nueva_imagen[2000 * 2000 * 3];
 
-void convertir(int in_fd, int out_fd, BMPHeader h, BMPInfoHeader infoh) 
+void convertir(int in_fd, int out_fd, BMPHeader h, BMPInfoHeader infoh)
 {
 
 	int width = infoh.width;
-	int height = infoh.height;
+	int height = infoh.height;//
 	unsigned char gray;
 
 	int padding = (4 - (width * 3) % 4) % 4; // Calculating padding size
 	unsigned char pixel[3];
+//for (int y = 2*(height /3); y < height; y++)
+	for (int y = 2*(height /3); y < height; y++)
+	{
+		for (int x = 0; x < width; x++)
+		{
 
-	for (int y = 0; y < height; y++) {
-		for (int x = 0; x < width; x++) {
 			read(in_fd, pixel, 3);
+
 			gray = (unsigned char)(0.3 * pixel[2] + 0.59 * pixel[1] + 0.11 * pixel[0]);
-			nueva_imagen[y*(width*3+padding) + x*3]= gray;
-			nueva_imagen[y*(width*3+padding) + x*3 + 1] = gray;
-			nueva_imagen[y*(width*3+padding) + x*3 + 2] = gray;
+			nueva_imagen[y * (width * 3 + padding) + x * 3] = gray;
+			nueva_imagen[y * (width * 3 + padding) + x * 3 + 1] = gray;
+			nueva_imagen[y * (width * 3 + padding) + x * 3 + 2] = gray;
 		}
 		lseek(in_fd, padding, SEEK_CUR); // Skipping padding
 	}
+
+	write(out_fd, &h, sizeof(BMPHeader));
+		write(out_fd, &infoh, sizeof(BMPInfoHeader));
+		write(out_fd, &nueva_imagen[0], infoh.width * infoh.height * 3);
 }
 
 void something_wrong(int fd, const char *m)
@@ -60,44 +73,56 @@ void something_wrong(int fd, const char *m)
 	if (fd > 0)
 		close(fd);
 	printf("%s\n", m),
-	exit(1);
+		exit(1);
 }
 
-int main() 
+int main()
 {
 	int in_fd = open(BMP_FILE, O_RDONLY);
-	if (in_fd < 0) {
+	if (in_fd < 0)
+	{
 		something_wrong(in_fd, "Error open");
 	}
 
 	BMPHeader h;
+	
 	read(in_fd, &h, sizeof(BMPHeader));
 
-	if (h.magic[0] != 'B' || h.magic[1] != 'M') {
+	if (h.magic[0] != 'B' || h.magic[1] != 'M')
+	{
 		something_wrong(in_fd, "No es BMP");
 	}
 
 	BMPInfoHeader infoh;
 	read(in_fd, &infoh, sizeof(BMPInfoHeader));
 
-	if (infoh.bpp != 24 || infoh.compression != 0) {
+	if (infoh.bpp != 24 || infoh.compression != 0)
+	{
 		something_wrong(in_fd, "No 24-bit BMP");
 	}
 
 	int out_fd = open(GRAYSCALE_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (out_fd < 0) {
+	if (out_fd < 0)
+	{
 		something_wrong(in_fd, "error en output");
 	}
 
-	convertir(in_fd, out_fd, h, infoh);
+	int pid1 = fork();
 
-	write(out_fd, &h, sizeof(BMPHeader));
-	write(out_fd, &infoh, sizeof(BMPInfoHeader));
-	write(out_fd, &nueva_imagen[0], infoh.width * infoh.height * 3);
+	if (pid1 == 0)
+	{
+		convertir(in_fd, out_fd, h, infoh);
+		
+		exit(0);
+	}else { 
+
+		
+	}
+
+	wait(pid1);
 
 	close(in_fd);
 	close(out_fd);
 	printf("Imagen en gris generada. %s\n", GRAYSCALE_FILE);
 	exit(0);
 }
-
--- TP/TP_4/prueba/prueba.c
+++ TP/TP_4/prueba/prueba.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
 #define BMP_FILE "input.bmp"
 #define GRAYSCALE_FILE "output_grayscale.bmp"
 
 #pragma pack(push, 1)
-typedef struct {
+typedef struct
+{
     unsigned char magic[2];
     unsigned int size;
     unsigned short reserved1;
@@ -16,7 +18,8 @@ typedef struct {
     unsigned int offset;
 } BMPHeader;
 
-typedef struct {
+typedef struct
+{
     unsigned int headerSize;
     int width;
     int height;
@@ -31,14 +34,16 @@ typedef struct {
 } BMPInfoHeader;
 #pragma pack(pop)
 
-unsigned char nueva_imagen[2000*2000*3];
+unsigned char nueva_imagen[2000 * 2000 * 3];
 
-void convertir(int in_fd, int start, int end, int width, int height, int padding) {
+void convertir(int in_fd, int start, int end, int width, int height, int padding, int out_fd) {
     unsigned char pixel[3];
     unsigned char gray;
 
     lseek(in_fd, start, SEEK_SET);
-    for (int y = start / (width * 3 + padding); y < end / (width * 3 + padding); y++) {
+    lseek(out_fd, start, SEEK_SET);
+
+    for (int y = start ; y < end ; y++) {
         for (int x = 0; x < width; x++) {
             read(in_fd, pixel, 3);
             gray = (unsigned char)(0.3 * pixel[2] + 0.59 * pixel[1] + 0.11 * pixel[0]);
@@ -48,82 +53,89 @@ void convertir(int in_fd, int start, int end, int width, int height, int padding
         }
         lseek(in_fd, padding, SEEK_CUR); // Skipping padding
     }
+
+    lseek(out_fd, start, SEEK_SET);
+    write(out_fd, &nueva_imagen[start], (end - start));
 }
 
-void something_wrong(int fd, const char *m) {
+void something_wrong(int fd, const char *m)
+{
     if (fd > 0)
         close(fd);
-    printf("%s\n", m),
+    printf("%s\n", m);
     exit(1);
 }
 
-int main() {
+int main()
+{
     int in_fd = open(BMP_FILE, O_RDONLY);
-    if (in_fd < 0) {
-        something_wrong(in_fd, "Error open");
+    if (in_fd < 0)
+    {
+        something_wrong(in_fd, "Error al abrir el archivo de entrada");
     }
 
     BMPHeader h;
     read(in_fd, &h, sizeof(BMPHeader));
 
-    if (h.magic[0] != 'B' || h.magic[1] != 'M') {
-        something_wrong(in_fd, "No es BMP");
+    if (h.magic[0] != 'B' || h.magic[1] != 'M')
+    {
+        something_wrong(in_fd, "El archivo no es un BMP válido");
     }
 
     BMPInfoHeader infoh;
     read(in_fd, &infoh, sizeof(BMPInfoHeader));
 
-    if (infoh.bpp != 24 || infoh.compression != 0) {
-        something_wrong(in_fd, "No 24-bit BMP");
+    if (infoh.bpp != 24 || infoh.compression != 0)
+    {
+        something_wrong(in_fd, "El BMP no es de 24 bits o está comprimido");
     }
 
     int width = infoh.width;
     int height = infoh.height;
     int padding = (4 - (width * 3) % 4) % 4;
 
-    // Calculate the start and end positions for each process
-    int section_height = height / 3;
-    int start1 = h.offset;
-    int end1 = start1 + section_height * (width * 3 + padding);
+    int start1 = sizeof(BMPHeader) + sizeof(BMPInfoHeader);
+    int end1 = start1 + height * (width * 3 + padding);
 
-    int start2 = end1;
-    int end2 = start2 + section_height * (width * 3 + padding);
+    int out_fd = open(GRAYSCALE_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    write(out_fd, &h, sizeof(BMPHeader));
+    write(out_fd, &infoh, sizeof(BMPInfoHeader));
 
-    int start3 = end2;
-    int end3 = start3 + section_height * (width * 3 + padding);
+    pid_t pid1, pid2, pid3;
 
-    // Fork processes
-    pid_t pid1, pid2;
-
-    if ((pid1 = fork()) == 0) {
-        convertir(in_fd, start1, end1, width, height, padding);
+    if ((pid1 = fork()) == 0)
+    {
+        convertir(in_fd, start1, end1, width, height, padding, out_fd);
         exit(0);
-    } else if (pid1 < 0) {
+    }
+    else if (pid1 < 0)
+    {
         something_wrong(in_fd, "Error en fork 1");
     }
 
-    if ((pid2 = fork()) == 0) {
-        convertir(in_fd, start2, end2, width, height, padding);
+    if ((pid2 = fork()) == 0)
+    {
+        convertir(in_fd, start1, end1, width, height, padding, out_fd);
         exit(0);
-    } else if (pid2 < 0) {
+    }
+    else if (pid2 < 0)
+    {
         something_wrong(in_fd, "Error en fork 2");
     }
 
-    // Main process handles the third part
-    convertir(in_fd, start3, end3, width, height, padding);
-
-    // Wait for child processes to finish
-    wait(NULL);
-    wait(NULL);
-
-    int out_fd = open(GRAYSCALE_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (out_fd < 0) {
-        something_wrong(in_fd, "Error en output");
+    if ((pid3 = fork()) == 0)
+    {
+        convertir(in_fd, start1, end1, width, height, padding, out_fd);
+        exit(0);
+    }
+    else if (pid3 < 0)
+    {
+        something_wrong(in_fd, "Error en fork 3");
     }
 
-    write(out_fd, &h, sizeof(BMPHeader));
-    write(out_fd, &infoh, sizeof(BMPInfoHeader));
-    //write(out_fd, &nueva_imagen[0], infoh.width * infoh.height * 3);
+    wait(NULL);
+    wait(NULL);
+    wait(NULL);
 
     close(in_fd);
     close(out_fd);
